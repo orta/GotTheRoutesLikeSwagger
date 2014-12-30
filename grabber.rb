1a268673d763a848d8fe4bea847189e8bd068d04
Orta Therox <orta.therox@gmail.com>
2014-12-30 22:31:23 +0000

    make more sense for non-me

--- grabber.rb
+++ grabber.rb
@@ -6,11 +6,14 @@ class Grabber
   def grab()
 
     root =  ARGV[0] || "https://api.artsy.net/api/v1/swagger_doc"
+
+    # temp token, will last a week
     token = ARGV[1] || "JvTPWe4WsQO-xqX6Bts49kAFbpDMRfxHtzyVZOdgDeRPYIpvqhke4GQZxHXJh3zTBd_TjZ7DTLfumZrTfS8iVIx9l9U3PVmTaTYOkiyPjVT-BozVkpXWTSQZIKYybNoavnv2wWNVmgCVqorndmD_xQUVoUMFU-IWrezae6XU0BPQBUhUHO8WTWkBVu9m0CCCm0jCd_mEWxKp8KUiEqBuwJai6RGGiZgFzBpFwpUuQQ4="
-    redownload = false
-    get_new_apis = false
+    redownload = true
+    get_new_apis = true
     
     if get_new_apis 
+      puts "Downloading all routes"
       get_all_apis =  REST.get(root, {})
       if get_all_apis.ok?
         response = JSON.parse(get_all_apis.body)
@@ -36,6 +39,7 @@ class Grabber
 
       elsif !redownload
       else
+        puts "Downloading all #{new_route}"
         endpoints_request =  REST.get(new_route, {})
         if endpoints_request.ok?
           route_apis = JSON.parse(endpoints_request.body)
